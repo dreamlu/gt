07fcfbf1e0ca369872366b1325efe3d8fbade768
dreamlu <3147460271@qq.com>
2019-06-09 14:24:19 +0800

    add mysql reconnect to solve docker/docker-compose start up prblem; up other;

--- db.go
+++ db.go
@@ -2,9 +2,11 @@
 package der
 
 import (
-	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
+	"strconv"
+	"time"
 )
 
 var (
@@ -17,7 +19,22 @@ func NewDB() {
 	DB, err = gorm.Open("mysql", GetDevModeConfig("db.user")+":"+GetDevModeConfig("db.password")+"@"+GetDevModeConfig("db.host")+"/"+GetDevModeConfig("db.name")+"?charset=utf8&parseTime=True&loc=Local")
 	//defer DB.Close()
 	if err != nil {
-		fmt.Println(err)
+		log.Println("[mysql连接错误]:", err)
+		log.Println("[mysql开始尝试重连中]: try it every 5s...")
+		// try to reconnect
+		for {
+			// go is so fast
+			// try it every 5s
+			time.Sleep(5 * time.Second)
+			DB, err = gorm.Open("mysql", GetDevModeConfig("db.user")+":"+GetDevModeConfig("db.password")+"@"+GetDevModeConfig("db.host")+"/"+GetDevModeConfig("db.name")+"?charset=utf8&parseTime=True&loc=Local")
+			//defer DB.Close()
+			if err != nil {
+				log.Println("[mysql连接错误]:", err)
+				continue
+			}
+			log.Println("[mysql重连成功]")
+			break
+		}
 	}
 	// Globally disable table names
 	// use name replace names
@@ -27,10 +44,21 @@ func NewDB() {
 	LogMode("debug") // or sqlErr
 
 	// connection pool
+	var maxIdle, maxOpen int
+	maxIdleConn := GetDevModeConfig("db.maxIdleConn")
+	if maxIdleConn == "" {
+		maxIdle = 20
+	}
+	maxIdle, _ = strconv.Atoi(maxIdleConn)
+
+	maxOpenConn := GetDevModeConfig("db.maxOpenConn")
+	if maxOpenConn == "" {
+		maxOpen = 100
+	}
+	maxOpen, _ = strconv.Atoi(maxIdleConn)
+
 	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
-	DB.DB().SetMaxIdleConns(20)
-
+	DB.DB().SetMaxIdleConns(maxIdle)
 	// SetMaxOpenConns sets the maximum number of open connections to the database.
-	DB.DB().SetMaxOpenConns(200)
+	DB.DB().SetMaxOpenConns(maxOpen)
 }
-
