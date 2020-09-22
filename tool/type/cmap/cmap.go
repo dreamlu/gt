1986d104fa0945c6579c6e9b848c1510616d7bba
cliod <qi2415@qq.com>
2020-09-22 18:03:39 +0800

    CMap中添加三个个方法, 获取,插入和追加

--- tool/type/cmap/cmap.go
+++ tool/type/cmap/cmap.go
@@ -3,6 +3,7 @@ package cmap
 import (
 	"encoding/json"
 	"net/url"
+	"strings"
 )
 
 type CMap map[string][]string
@@ -47,6 +48,34 @@ func (v CMap) Add(key, value string) CMap {
 func (v CMap) Del(key string) CMap {
 	delete(v, key)
 	return v
+}
+
+// Obtain get all values associated with the given key.
+func (v CMap) Obtain(key string) []string {
+	if v == nil {
+		return []string{}
+	}
+	return v[key]
+}
+
+// Append set the key to value if it doesn't exists. append if it exists.
+func (v CMap) Append(key, value string) CMap {
+	vs := v.Get(key)
+	if vs == "" || len(strings.Trim(vs, " ")) == 0 {
+		v.Set(key, value)
+		return v
+	}
+	return v.Set(key, vs+value)
+}
+
+// Append set the key to value if it doesn't exists. insert if it exists.
+func (v CMap) Insert(key, value string) CMap {
+	vs := v.Get(key)
+	if vs == "" || len(strings.Trim(vs, " ")) == 0 {
+		v.Set(key, value)
+		return v
+	}
+	return v.Set(key, value+vs)
 }
 
 // CMap to struct data
