36afea62c5d80129769e7575cbedcda5018ae47e
dreamlu <3147460271@qq.com>
2021-05-22 16:21:34 +0800

    remove uitl/ip and gsync test; up TODO.md

--- tool/gsync/gsync_test.go
+++ tool/gsync/gsync_test.go
@@ -2,6 +2,8 @@ package gsync
 
 import (
 	"fmt"
+	"net/http"
+	_ "net/http/pprof"
 	"runtime"
 	"sync"
 	"testing"
@@ -45,6 +47,24 @@ func TestPool_SubmitWait(t *testing.T) {
 		}
 
 		t.Log(p.SubmitWait(readWait))
+	}
+}
+
+func TestPool_Submit(t *testing.T) {
+
+	t.SkipNow()
+	read := func() {
+		fmt.Printf("go func time: %d\n", time.Now().Unix())
+		time.Sleep(time.Second * 10)
+	}
+
+	for i := 0; i < 1000; i++ {
+		t.Log(i)
+		p.Submit(read)
+	}
+	ip := "0.0.0.0:9000"
+	if err := http.ListenAndServe(ip, nil); err != nil {
+		fmt.Printf("start pprof failed on %s\n", ip)
 	}
 }
 
