d34143607523c4a6321a9213ccaa9e76929f6dbf
dreamlu <3147460271@qq.com>
2022-07-05 09:05:20 +0800

    update readme

--- README.md
+++ README.md
@@ -6,7 +6,29 @@ everything is a tool 一切皆工具
 
 It will help you...
 
-#### demo  
-- [gt-crud](https://github.com/dreamlu/gt-crud)   
-- [micro-go](https://github.com/dreamlu/micro-go)   
-- lots of private projects  
+#### pkg
+
+| pkg         | 作用       |
+|-------------|:---------|
+| cache       | 缓存       |
+| conf        | yaml配置   |
+| crud        | 丰富的crud  |
+| log         | 日志库      |
+| mock        | 模拟数据     |
+| mongo       | nosql    |
+| mq          | 消息队列     |
+| snowflake   | 雪花算法唯一id |
+| [src](#src) | 官方库扩展    |
+
+#### src
+
+| src     | 作用         |
+|---------|:-----------|
+| file    | 文件处理       |
+| ghttp   | http简化易用处理 |
+| gos     | os扩展       |
+| gsync   | 携程池与实时进程   |
+| reflect | 反射扩展       |
+| type    | 常用自定义类型    |
+| util    | util工具     |
+| valid   | 自定义字段验证    |
--- conf/conf.go
+++ conf/conf.go
@@ -1,7 +1,5 @@
 package conf
 
-import "sync"
-
 type Config struct {
 	// different devMode yaml data
 	YamlS []*Yaml
@@ -10,35 +8,30 @@ type Config struct {
 }
 
 var (
-	onceConfig sync.Once
-	// global log
+	// global config
 	config *Config
 )
 
-// Configger single config
-func Configger(params ...string) *Config {
-
-	onceConfig.Do(func() {
-		config = NewConfig(params[:]...)
-	})
-	return config
+// 设计模式--单例模式[饿汉式]
+func init() {
+	config = NewConfig()
 }
 
 type value interface {
-	int | string | bool | float64
+	int64 | int | string | bool | float64
 }
 
 func Get[T value](name string) (t T) {
-	if v := Configger().Get(name); v != nil {
+	if v := config.Get(name); v != nil {
 		return v.(T)
 	}
 	return
 }
 
 func UnmarshalField(name string, v any) {
-	Configger().UnmarshalField(name, v)
+	config.UnmarshalField(name, v)
 }
 
 func Unmarshal(v any) {
-	Configger().Unmarshal(v)
+	config.Unmarshal(v)
 }
