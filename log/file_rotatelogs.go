bb44ceb2a308d5abb0fa2570450d527146bbcd04
dreamlu <3147460271@qq.com>
2025-08-11 16:04:25 +0800

    add cleanOldLogs for always restart program

--- log/file_rotatelogs.go
+++ log/file_rotatelogs.go
@@ -3,16 +3,25 @@ package log
 import (
 	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
 	"go.uber.org/zap/zapcore"
+	"os"
 	"path"
+	"path/filepath"
+	"sync"
 	"time"
 )
 
 var FileRotatelogs = new(fileRotatelogs)
 
-type fileRotatelogs struct{}
+type fileRotatelogs struct {
+	once sync.Once
+}
 
 func (r *fileRotatelogs) GetWriteSyncer(level string) (zapcore.WriteSyncer, error) {
-	// 这里只返回文件写入器，不返回 MultiWriteSyncer
+
+	r.once.Do(func() {
+		r.cleanOldLogs(confLogDirector, confLogMaxAge)
+	})
+
 	fileWriter, err := r.GetFileWriter(level)
 	return zapcore.AddSync(fileWriter), err
 }
@@ -25,4 +34,22 @@ func (r *fileRotatelogs) GetFileWriter(level string) (*rotatelogs.RotateLogs, er
 		rotatelogs.WithRotationTime(time.Hour*24),
 	)
 	return fileWriter, err
+}
+
+// cleanOldLogs run on the start
+func (r *fileRotatelogs) cleanOldLogs(dir string, maxAgeDays int) {
+	cutoff := time.Now().Add(-time.Duration(maxAgeDays) * 24 * time.Hour)
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return nil
+		}
+		if info.IsDir() {
+			if t, err := time.Parse("2006-01-02", info.Name()); err == nil {
+				if t.Before(cutoff) {
+					_ = os.RemoveAll(path)
+				}
+			}
+		}
+		return nil
+	})
 }
